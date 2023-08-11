b1c0675463f3bb1b4ab8a8ed3d29270ecbbca865
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2023-08-11 12:36:25 +0700

    Component - Pagination

--- src/components/PaginationBar/PaginationBar.js
+++ src/components/PaginationBar/PaginationBar.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import styles from './PaginationBar.module.sass'
 import classNames from 'classnames/bind'
+import Pagination from 'react-bootstrap/Pagination';
 const cx = classNames.bind(styles)
 
-function PaginationBar() {
+function PaginationBar({ pages }) {
+
+    let active = 1
+    let items = [];
+    for (let number = 1; number <= pages; number++) {
+        items.push(
+            <Pagination.Item key={number} active={number === active}>
+                {number}
+            </Pagination.Item>,
+        );
+    }
     return (
-        <h1>pagination</h1>
-    )
-} 
+        <Pagination size="lg">
+            <Pagination.First />
+            <Pagination.Prev />
+            {items}
+            <Pagination.Next />
+            <Pagination.Last />
+        </Pagination>
+
+    );
+
+}
+export default PaginationBar
