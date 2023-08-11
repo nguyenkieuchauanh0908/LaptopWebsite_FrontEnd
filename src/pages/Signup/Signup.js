2b805b6eb046e8592e6f62bcee0f2b8928a7a7e4
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2023-08-11 17:47:57 +0700

    Signup page - check input format

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -3,22 +3,25 @@ import { useState } from 'react';
 import styles from './Signup.module.scss'
 import classNames from 'classnames/bind';
 import { GoogleIcon, FacebookIcon } from '../../components/Icons';
+import { useNavigate } from 'react-router-dom';
 
 const cx = classNames.bind(styles)
 
 function Signup() {
 
+    const [fName, setFName] = useState('')
+    const [lName, setLName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleEmail = (email) => {
-        setEmail(email)
-        return checkEmailValid(email)
-    }
-    const handlePassword = (password) => {
-        setPassword(password)
-        return checkPasswordComplexity(password)
-    }
+    const [error, setError] = useState('')
+    const [sucessMessage, setSucessMessage] = useState('');
+    const [fNameError, setFNameError] = useState('')
+    const [lNameError, setLNameError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+    const [mailError, setMailError] = useState('')
+
+
 
     const checkEmailValid = (email) => {
         const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -54,38 +57,84 @@ function Signup() {
         return true;
     }
 
+    const hasDiacritics = (name) => {
+        // Regular expression to match diacritics
+        const regex = /[\u0300-\u036f]/g;
 
+        return regex.test(name);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setError('')
+        setMailError('')
+        setPasswordError('')
+        setFNameError('')
+        setLNameError('')
+        if (fName !== '' && lName !== '' && password !== '' && email !== '') {
+            if (checkEmailValid(email) && checkPasswordComplexity(password) && !hasDiacritics(fName) && !hasDiacritics(lName)) {
+                setSucessMessage('Đăng ký thành công')
+                setError('')
+                setMailError('')
+                setPasswordError('')
+                setFNameError('')
+                setLNameError('')
+            }
+            else {
+                setSucessMessage('')
+                if (!checkEmailValid(email))
+                    setMailError('Email không đúng định dạng!')
+                if (!checkPasswordComplexity(password))
+                    setPasswordError('Mật khẩu dài ít nhất 8 ký tự, tối đa 20 ký tự, phải bao gồm số, chữ hoa, chữ thường và ký tự đặc biệt')
+                if (hasDiacritics(fName))
+                    setFNameError('Vui lòng dùng chữ không dấu')
+                if (hasDiacritics(lName))
+                    setLNameError('Vui lòng dùng chữ không dấu')
+            }
+        } else {
+
+            setError('Vui lòng điền đầy đủ thông tin!')
+        }
+
+    }
 
     return (
         <div className={cx('form-wrapper')}>
-            <form className={cx('loginForm')}>
+            <form className={cx('loginForm')} onSubmit={handleSubmit}>
                 <p className={cx('form-title')}>Đăng ký</p>
                 <div className={cx('input-wrapper')}>
                     <div>
                         <div className={cx('input-wrapper-item')}>
                             <label className={cx('form-label')} for="fName"> Họ </label>
-                            <input className={cx('form-input')} type="text" id="fName" name="fName" placeholder='Nhập họ'></input>
+                            <input className={cx('form-input')} type="text" id="fName" name="fName" placeholder='Nhập họ' onChange={(e) => setFName(e.target.value)}></input>
+                            {fNameError && <p className={cx('form-error-message')}>{fNameError}</p>}
                         </div>
                         <div className={cx('input-wrapper-item')}>
                             <label className={cx('form-label')} for="lName"> Tên </label>
-                            <input className={cx('form-input')} type="text" id="lName" name="lName" placeholder='Nhập tên'></input>
+                            <input className={cx('form-input')} type="text" id="lName" name="lName" placeholder='Nhập tên' onChange={(e) => setLName(e.target.value)}></input>
+                            {lNameError && <p className={cx('form-error-message')}>{lNameError}</p>}
                         </div>
                     </div>
 
                     <div className={cx('input-wrapper-item')}>
                         <label className={cx('form-label')} for="email"> Email </label>
-                        <input className={cx('form-input')} type="text" id="email" name="email" placeholder='Nhập email'></input>
+                        <input className={cx('form-input')} type="text" id="email" name="email" placeholder='Nhập email' onChange={(e) => setEmail(e.target.value)}></input>
+                        {mailError && <p className={cx('form-error-message')}>{mailError}</p>}
                     </div>
                     <div className={cx('input-wrapper-item')}>
                         <label className={cx('form-label')} for="pw">Mật khẩu</label>
-                        <input className={cx('form-input')} type="password" id="pw" name="pw" placeholder='Nhập mật khẩu'></input>
+                        <input className={cx('form-input')} type="password" id="pw" name="pw" placeholder='Nhập mật khẩu' onChange={(e) => setPassword(e.target.value)}></input>
+                        {passwordError && <p className={cx('form-error-message')}>{passwordError}</p>}
                     </div>
+                    {sucessMessage && <p className={cx('form-sucess-message')}>{sucessMessage}</p>}
+                    {error && <p className={cx('form-error-message')}>{error}</p>}
+
                 </div>
                 <div className={cx('redirect-options', 'option-SignUp')}>
                     <p>Đã có tài khoản? <a href=''>Đăng nhập ngay</a></p>
                 </div>
                 <div className={cx('btns-group-control')}>
-                    <div className={cx('btn', 'btn-SignIn')} type="button">Đăng ký</div>
+                    <button className={cx('btn', 'btn-SignIn')} type="submit" >Đăng ký</button>
                     <p className={cx('options-title')}>-Or-</p>
                     <div className={cx('option-signIn')}>
                         <div className={cx('btn', 'btn-SignIn-google')} type="button">
