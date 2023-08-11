aa1cc543888739a68219ed34f44811f17a322d37
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2023-08-11 16:34:38 +0700

    Log in page - Check input

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,48 +1,90 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Login.module.scss'
 import classNames from 'classnames/bind';
 import { GoogleIcon, FacebookIcon } from '../../components/Icons';
+import { useNavigate } from 'react-router-dom';
 
 const cx = classNames.bind(styles)
 
 function Login() {
-    return (
-        <div className={cx('form-wrapper')}>
-            <form className={cx('loginForm')}>
-                <p className={cx('form-title')}>Đăng nhập</p>
-                <div className={cx('input-wrapper')}>
-                    <div className={cx('input-wrapper-item')}>
-                        <label className={cx('form-label')} for="email"> Email </label>
-                        <input className={cx('form-input')} type="text" id="email" name="email" placeholder='Nhâp email'></input>
-                    </div>
-                    <div className={cx('input-wrapper-item')}>
-                        <label className={cx('form-label')} for="pw">Mật khẩu</label>
-                        <input className={cx('form-input')} type="password" id="pw" name="pw" placeholder='Nhập mật khẩu'></input>
-                    </div>
-                </div>
-                <div className={cx('redirect-options', 'option-SignUp')}>
-                    <p>Chưa có tài khoản? <a href=''>Đăng ký ngay</a></p>
-                </div>
-                <div className={cx('btns-group-control')}>
-                    <div className={cx('btn', 'btn-SignIn')} type="button">Đăng nhập</div>
-                    <p className={cx('options-title')}>-Or-</p>
-                    <div className={cx('option-signIn')}>
-                        <div className={cx('btn', 'btn-SignIn-google')} type="button">
-                            <GoogleIcon />
-                            Đăng nhập bằng Gmail
-                        </div>
-                        <div className={cx('btn', 'btn-SignIn-facebook')} type="button">
-                            <FacebookIcon width='24' height='24' />
-                            Đăng nhập bằng Facebook
-                        </div>
-                    </div>
 
-                </div>
-                <div className={cx('redirect-options', 'option-forgetPw')}>
-                    <p>Quên mật khẩu? <a href=''>Đặt lại mật khẩu ngay</a></p>
-                </div>
-            </form>
-        </div>
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //Simple login authentication
+        if (email !== '' && password !== '') {
+            if (email === 'email@example.com' && password === 'password') {
+                setIsLoggedIn(true);
+
+            }
+            else {
+                setError('Email hoặc mật khẩu không đúng')
+            }
+        }
+        else {
+            if (email === '')
+                setError('Vui lòng điền email')
+            if (password === '')
+                setError('Vui lòng điền mật khẩu')
+            if (email === '' && password === '')
+                setError('Vui lòng điền đủ email và mật khẩu')
+        }
+
+
+    }
+    return (
+        <>
+            {
+                isLoggedIn ? navigate('/') : (
+                    <div className={cx('form-wrapper')}>
+                        <form className={cx('loginForm')} onSubmit={handleSubmit}>
+                            <p className={cx('form-title')}>Đăng nhập</p>
+                            <div className={cx('input-wrapper')}>
+                                <div className={cx('input-wrapper-item')}>
+                                    <label className={cx('form-label')} for="email"> Email </label>
+                                    <input className={cx('form-input')} type="text" id="email" autoComplete='on' name="email" placeholder='Nhập email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                                </div>
+                                <div className={cx('input-wrapper-item')}>
+                                    <label className={cx('form-label')} for="pw">Mật khẩu</label>
+                                    <input className={cx('form-input')} type="password" id="pw" name="pw" autocomplete='on' placeholder='Nhập mật khẩu' value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                                    {error && <p className={cx('form-error-message')}>{error}</p>}
+                                </div>
+                            </div>
+                            <div className={cx('redirect-options', 'option-SignUp')}>
+                                <p>Chưa có tài khoản? <a href='#'>Đăng ký ngay</a></p>
+                            </div>
+                            <div className={cx('btns-group-control')}>
+                                <button className={cx('btn', 'btn-SignIn')} type="submit">Đăng nhập</button>
+                                <p className={cx('options-title')}>-Or-</p>
+                                <div className={cx('option-signIn')}>
+                                    <button className={cx('btn', 'btn-SignIn-google')}>
+                                        <GoogleIcon />
+                                        Đăng nhập bằng Gmail
+                                    </button>
+                                    <button className={cx('btn', 'btn-SignIn-facebook')}>
+                                        <FacebookIcon width='24' height='24' />
+                                        Đăng nhập bằng Facebook
+                                    </button>
+                                </div>
+
+                            </div>
+                            <div className={cx('redirect-options', 'option-forgetPw')}>
+                                <p>Quên mật khẩu? <a href='#'>Đặt lại mật khẩu ngay</a></p>
+                            </div>
+                        </form>
+                    </div>
+                )
+            }
+
+        </>
+
 
     )
 }
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './Signup.module.scss'
 import classNames from 'classnames/bind';
 import { GoogleIcon, FacebookIcon } from '../../components/Icons';
@@ -6,6 +7,55 @@ import { GoogleIcon, FacebookIcon } from '../../components/Icons';
 const cx = classNames.bind(styles)
 
 function Signup() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmail = (email) => {
+        setEmail(email)
+        return checkEmailValid(email)
+    }
+    const handlePassword = (password) => {
+        setPassword(password)
+        return checkPasswordComplexity(password)
+    }
+
+    const checkEmailValid = (email) => {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(email);
+    }
+
+    const checkPasswordComplexity = (password) => {
+        // Password should be at least 8 characters long
+        if (password.length < 8) {
+            return false;
+        }
+
+        // Password should contain at least one uppercase letter
+        if (!/[A-Z]/.test(password)) {
+            return false;
+        }
+
+        // Password should contain at least one lowercase letter
+        if (!/[a-z]/.test(password)) {
+            return false;
+        }
+
+        // Password should contain at least one digit
+        if (!/\d/.test(password)) {
+            return false;
+        }
+
+        // Password should contain at least one special character
+        if (!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password)) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
     return (
         <div className={cx('form-wrapper')}>
             <form className={cx('loginForm')}>
