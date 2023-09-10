138611e2e2c20729a131a03fb0a240da2d5c9f98
mytran324 <92320923+mytran324@users.noreply.github.com>
2023-09-10 20:37:12 +0700

    móc update api profileAdmin, orderAdmin, employeeManager

--- src/pages/Admin/ProfileAdmin/ProfileAdmin.js
+++ src/pages/Admin/ProfileAdmin/ProfileAdmin.js
@@ -8,22 +8,88 @@ import * as profileAdminService from '../../../services/profileAdminService';
 
 const cx = classNames.bind(styles);
 function ProfileAdmin() {
-    const userId = '64b7eb3570a3229b5506a22f';
+    const userId = '64b8b48d116933190a3d3543';
+    const [reloadData, setReloadData] = useState(true);
     const [user, setUser] = useState([]);
+    const [fname, setFname] = useState([]);
+    const [lname, setLname] = useState([]);
+    const [email, setEmail] = useState([]);
+    const [phones, setPhones] = useState([]);
+    const [addresses, setAddresses] = useState([]);
+    const [dateOfBirth, setDateOfbirth] = useState([]);
+    const [gender, setGender] = useState([]);
+
     useEffect(() => {
         const fetchApi = async () => {
             const result = await profileAdminService.getUser(userId);
             setUser(result);
+            setFname(result._fname);
+            setLname(result._lname);
+            setEmail(result._email);
+            setPhones(result._phones);
+            setAddresses(result._addresses);
+            setDateOfbirth(result._dateOfBirth);
+            setGender(result._gender);
         };
-        fetchApi();
-    }, []);
+        if (reloadData) {
+            fetchApi();
+            setReloadData(false); // Đặt lại state để ngăn việc gọi lại liên tục
+        }
+    }, [reloadData]);
+    //Form cập nhật thông tin
     const [updateProfile, setUpdateProfile] = useState(false);
     const showUpdateModal = () => setUpdateProfile(true);
     const closeUpdateModal = () => setUpdateProfile(false);
-
+    const handleSaveChangeProfile = async () => {
+        const updateProfileData = {
+            _fname: fname,
+            _lname: lname,
+            _email: email,
+            _phones: phones[0],
+            _addresses: addresses[0],
+            _dateOfBirth: dateOfBirth,
+            _gender: gender,
+        };
+        const result = await profileAdminService.updateProfile(userId, updateProfileData);
+        if (result === 1) {
+            setReloadData(true);
+            closeUpdateModal();
+            window.alert('Cập nhật thông tin thành công');
+        }
+    };
+    //Form cập nhật mật khẩu
     const [changePassword, setChangePassword] = useState(false);
-    const showChangePassWordModal = () => setChangePassword(true);
+    const showChangePassWordModal = () => {
+        setChangePassword(true);
+    };
     const closeChangePassWordModal = () => setChangePassword(false);
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmNewPassword, setConfirmNewPassword] = useState('');
+    const handleSaveChangePassword = async () => {
+        if (newPassword === confirmNewPassword) {
+            if (oldPassword === user._pw) {
+                const saveChangePassword = await profileAdminService.changePassword(userId, { _pw: newPassword });
+                if (saveChangePassword === 1) {
+                    setOldPassword('');
+                    setNewPassword('');
+                    setConfirmNewPassword('');
+                    setReloadData(true);
+                    closeChangePassWordModal();
+                    window.alert('Cập nhật mật khẩu thành công!');
+                }
+            } else {
+                window.alert('Mật khẩu không chính xác!');
+            }
+        } else {
+            window.alert('Nhập lại mật khẩu không khớp!');
+        }
+    };
+
+    //From cập nhật avatar
+    const [changeAvatar, setChangeAvatar] = useState(false);
+    const showChangeAvatarModal = () => setChangeAvatar(true);
+    const closeChangeAvatarModal = () => setChangeAvatar(false);
 
     return (
         <div className={cx('container-fluid')}>
@@ -64,7 +130,7 @@ function ProfileAdmin() {
                                                 <div>Số điện thoại:</div>
                                             </div>
                                             <div className={cx('col-lg-9 col-md-9 d-flex justify-content-end', 'info')}>
-                                                <div>{user._phones}</div>
+                                                <div>{phones[0]}</div>
                                             </div>
                                         </div>
                                         <div className={cx('row')}>
@@ -72,7 +138,7 @@ function ProfileAdmin() {
                                                 <div>Địa chỉ:</div>
                                             </div>
                                             <div className={cx('col-lg-9 col-md-9 d-flex justify-content-end', 'info')}>
-                                                <div>{user._addresses}</div>
+                                                <div>{addresses[0]}</div>
                                             </div>
                                         </div>
                                         <div className={cx('row')}>
@@ -102,7 +168,11 @@ function ProfileAdmin() {
                                             </div>
                                         </div>
                                         <div className={cx('d-flex justify-content-center', 'change-avatar-btn')}>
-                                            <button type="button" class="btn btn-primary btn-lg">
+                                            <button
+                                                type="button"
+                                                class="btn btn-primary btn-lg"
+                                                onClick={showChangeAvatarModal}
+                                            >
                                                 Đổi ảnh
                                             </button>
                                         </div>
@@ -131,7 +201,11 @@ function ProfileAdmin() {
                                             </div>
                                         </div>
                                         <div className={cx('d-flex justify-content-center', 'change-avatar-btn')}>
-                                            <button type="button" class="btn btn-primary btn-lg">
+                                            <button
+                                                type="button"
+                                                class="btn btn-primary btn-lg"
+                                                onClick={showChangeAvatarModal}
+                                            >
                                                 Đổi ảnh
                                             </button>
                                         </div>
@@ -174,7 +248,7 @@ function ProfileAdmin() {
                                                 <div>Số điện thoại:</div>
                                             </div>
                                             <div className={cx('col-8 col-md-9 d-flex justify-content-end', 'info')}>
-                                                <div>{user._phones}</div>
+                                                <div>{phones[0]}</div>
                                             </div>
                                         </div>
                                         <div className={cx('row')}>
@@ -182,7 +256,7 @@ function ProfileAdmin() {
                                                 <div>Địa chỉ:</div>
                                             </div>
                                             <div className={cx('col-8 col-md-9 d-flex justify-content-end', 'info')}>
-                                                <div>{user._addresses}</div>
+                                                <div>{addresses[0]}</div>
                                             </div>
                                         </div>
                                         <div className={cx('row')}>
@@ -215,46 +289,90 @@ function ProfileAdmin() {
                                 <div className={cx('col-lg-3 col-md-3', 'heading-modal')}>
                                     <div>Họ:</div>
                                 </div>
-                                <input type="text" className={cx('col-lg-9 col-md-9')} />
+                                <input
+                                    type="text"
+                                    defaultValue={fname}
+                                    onChange={(e) => setFname(e.target.value)}
+                                    className={cx('col-lg-9 col-md-9')}
+                                />
                             </div>
                             <div className={cx('row align-items-center')}>
                                 <div className={cx('col-lg-3 col-md-3', 'heading-modal')}>
                                     <div>Tên:</div>
                                 </div>
-                                <input type="text" className={cx('col-lg-9 col-md-9')} />
+                                <input
+                                    type="text"
+                                    defaultValue={lname}
+                                    onChange={(e) => setLname(e.target.value)}
+                                    className={cx('col-lg-9 col-md-9')}
+                                />
                             </div>
                             <div className={cx('row align-items-center')}>
                                 <div className={cx('col-lg-3 col-md-3', 'heading-modal')}>
                                     <div>Email:</div>
                                 </div>
-                                <input type="email" className={cx('col-lg-9 col-md-9')} />
+                                <input
+                                    type="email"
+                                    defaultValue={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className={cx('col-lg-9 col-md-9')}
+                                />
                             </div>
                             <div className={cx('row align-items-center')}>
                                 <div className={cx('col-lg-3 col-md-3', 'heading-modal')}>
                                     <div>Số điện thoại:</div>
                                 </div>
-                                <input type="text" className={cx('col-lg-9 col-md-9')} />
+                                <input
+                                    type="text"
+                                    defaultValue={phones[0]}
+                                    onChange={(e) => setPhones(e.target[0].value)}
+                                    className={cx('col-lg-9 col-md-9')}
+                                />
                             </div>
                             <div className={cx('row align-items-center')}>
                                 <div className={cx('col-lg-3 col-md-3', 'heading-modal')}>
                                     <div>Địa chỉ:</div>
                                 </div>
-                                <input type="text" className={cx('col-lg-9 col-md-9')} />
+                                <input
+                                    type="text"
+                                    defaultValue={addresses[0]}
+                                    onChange={(e) => setAddresses(e.target[0].value)}
+                                    className={cx('col-lg-9 col-md-9')}
+                                />
                             </div>
                             <div className={cx('row align-items-center')}>
                                 <div className={cx('col-lg-3 col-md-3', 'heading-modal')}>
                                     <div>Ngày sinh:</div>
                                 </div>
-                                <input type="date" className={cx('col-lg-9 col-md-9')} />
+                                <input
+                                    type="date"
+                                    defaultValue={dateOfBirth}
+                                    onChange={(e) => setDateOfbirth(e.target.value)}
+                                    className={cx('col-lg-9 col-md-9')}
+                                />
                             </div>
                             <div className={cx('row align-items-center')}>
                                 <div className={cx('col-lg-3 col-md-3', 'heading-modal')}>
                                     <div>Giới tính:</div>
                                 </div>
                                 <span className={cx('col-lg-9 col-md-9', 'gender')}>
-                                    <input type="radio" id="male" name="gender" value="male" />
+                                    <input
+                                        type="radio"
+                                        id="male"
+                                        name="gender"
+                                        value="male"
+                                        checked={gender === 'male'}
+                                        onChange={(e) => setGender(e.target.value)}
+                                    />
                                     <label for="male">Nam</label>
-                                    <input type="radio" id="female" name="gender" value="female" />
+                                    <input
+                                        type="radio"
+                                        id="female"
+                                        name="gender"
+                                        value="female"
+                                        checked={gender === 'female'}
+                                        onChange={(e) => setGender(e.target.value)}
+                                    />
                                     <label for="female">Nữ</label>
                                 </span>
                             </div>
@@ -263,7 +381,7 @@ function ProfileAdmin() {
                             <button className={cx('btn btn-secondary btn-lg')} onClick={closeUpdateModal}>
                                 Đóng
                             </button>
-                            <button className={cx('btn btn-primary btn-lg')} onClick={closeUpdateModal}>
+                            <button className={cx('btn btn-primary btn-lg')} onClick={handleSaveChangeProfile}>
                                 Lưu thay đổi
                             </button>
                         </Modal.Footer>
@@ -281,26 +399,54 @@ function ProfileAdmin() {
                                 <div className={cx('col-lg-5 col-md-5', 'heading-modal')}>
                                     <div>Mật khẩu:</div>
                                 </div>
-                                <input type="password" className={cx('col-lg-7 col-md-7')} />
+                                <input
+                                    type="password"
+                                    onChange={(e) => setOldPassword(e.target.value)}
+                                    className={cx('col-lg-7 col-md-7')}
+                                />
                             </div>
                             <div className={cx('row align-items-center')}>
                                 <div className={cx('col-lg-5 col-md-5', 'heading-modal')}>
                                     <div>Mật khẩu mới:</div>
                                 </div>
-                                <input type="password" className={cx('col-lg-7 col-md-7')} />
+                                <input
+                                    type="password"
+                                    onChange={(e) => setNewPassword(e.target.value)}
+                                    className={cx('col-lg-7 col-md-7')}
+                                />
                             </div>
                             <div className={cx('row align-items-center')}>
                                 <div className={cx('col-lg-5 col-md-5', 'heading-modal')}>
                                     <div>Nhập lại mật khẩu mới:</div>
                                 </div>
-                                <input type="password" className={cx('col-lg-7 col-md-7')} />
+                                <input
+                                    type="password"
+                                    onChange={(e) => setConfirmNewPassword(e.target.value)}
+                                    className={cx('col-lg-7 col-md-7')}
+                                />
                             </div>
                         </Modal.Body>
                         <Modal.Footer>
                             <button className={cx('btn btn-secondary btn-lg')} onClick={closeChangePassWordModal}>
                                 Đóng
                             </button>
-                            <button className={cx('btn btn-primary btn-lg')} onClick={closeChangePassWordModal}>
+                            <button className={cx('btn btn-primary btn-lg')} onClick={handleSaveChangePassword}>
+                                Lưu thay đổi
+                            </button>
+                        </Modal.Footer>
+                    </Modal>
+                    <Modal show={changeAvatar} onHide={closeChangeAvatarModal} className={cx('change-avatar-modal')}>
+                        <Modal.Header closeButton>
+                            <div className={cx('title-modal')}>Đổi ảnh</div>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <input type="file" id="fileInput" name="fileInput" accept="image/*" />
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <button className={cx('btn btn-secondary btn-lg')} onClick={closeChangeAvatarModal}>
+                                Đóng
+                            </button>
+                            <button className={cx('btn btn-primary btn-lg')} onClick={closeChangeAvatarModal}>
                                 Lưu thay đổi
                             </button>
                         </Modal.Footer>
--- src/services/orderAdminService.js
+++ src/services/orderAdminService.js
@@ -12,10 +12,30 @@ export const getAllOrders = async () => {
 
 export const getOrder = async (orderId) => {
     try {
-        const res = await httprequest.get(`/admin/orders/${orderId}`);
+        const res = await httprequest.get(`/admin/orders/order-detail/`, {
+            params: {
+                orderId,
+            },
+        });
+        console.log(res);
         return res;
     } catch (error) {
         console.error(error);
         throw new Error('Lỗi trong quá trình lấy đơn hàng');
+    }
+};
+
+export const updateOrder = async (orderId, data) => {
+    try {
+        const res = await httprequest.put(`/admin/orders/update-order/`, data, {
+            params: {
+                orderId,
+            },
+        });
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Lỗi trong quá trình cập nhật đơn hàng');
     }
 };
