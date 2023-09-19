7df3f3c6fd249649589d6f306fdc98a25179ffbb
Thien Thach <vnsnake123@gmail.com>
2023-09-20 01:31:57 +0700

    up list order

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,5 +1,34 @@
 import * as httprequest from '../utils/httprequest';
 
+const getOrder = async () => {
+    const token = localStorage.getItem('token');
+    try {
+        const response = await httprequest.get('/orders', {
+            headers: {
+                Authorization: `Bearer ${token}`, // Gửi token trong header
+            },
+        });
+        return response;
+    } catch (error) {
+        console.log('Lỗi lấy danh sách đơn hàng:', error);
+        throw new Error('Đã xảy ra lỗi lấy danh sách đơn hàng');
+    }
+};
+const getOrderByStatus = async (status) => {
+    const token = localStorage.getItem('token');
+    try {
+        const response = await httprequest.get(`/orders/${status}`, {
+            headers: {
+                Authorization: `Bearer ${token}`, // Gửi token trong header
+            },
+        });
+        return response;
+    } catch (error) {
+        console.log('Lỗi lấy danh sách đơn hàng:', error);
+        throw new Error('Đã xảy ra lỗi lấy danh sách đơn hàng');
+    }
+};
+
 const postOrder = async (orderData) => {
     try {
         const response = await httprequest.post('/orders/create', orderData);
@@ -7,6 +36,27 @@ const postOrder = async (orderData) => {
     } catch (error) {
         console.log('Lỗi khi đặt hàng:', error);
         throw new Error('Đã xảy ra lỗi khi đặt hàng');
+    }
+};
+const cancelOrder = async (id) => {
+    const token = localStorage.getItem('token');
+    console.log('====================================');
+    console.log('Token ', token);
+    console.log('====================================');
+    try {
+        const response = await httprequest.put(
+            `/orders/cancel/${id}`,
+            {},
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`, // Gửi token trong header
+                },
+            },
+        );
+        return response;
+    } catch (error) {
+        console.log('Lỗi khi hủy đơn:', error);
+        throw new Error('Đã xảy ra lỗi khi hủy đơn hàng');
     }
 };
 
@@ -23,4 +73,4 @@ const postOrderVnpay = async (orderData) => {
     }
 };
 
-export { postOrder, postOrderVnpay }; // Export hàm postOrder
+export { postOrder, postOrderVnpay, getOrder, getOrderByStatus, cancelOrder }; // Export hàm postOrder
